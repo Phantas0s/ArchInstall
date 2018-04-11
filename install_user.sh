baeba2a1b65c102e54233a4603441fdf03c359c9
Matthieu <Matthieu.cneude@gmail.com>
2018-04-11 06:45:43 +0200

    [master] Fix dotfiles install

--- install_user.sh
+++ install_user.sh
@@ -34,8 +34,8 @@ dialog --infobox "[$(whoami)] Downloading and installing dotfiles..." 10 60
 git clone https://github.com/Phantas0s/.dotfiles.git /home/$(whoami)/.dotfiles >/dev/null \
     && source /home/$(whoami)/.dotfiles/env >/dev/null \
     && cp /home/$(whoami)/.dotfiles/install_config.diff /home/$(whoami)/.dotfiles/install_config >/dev/null \
-    && sh /home/$(whoami)/.dotfiles/install.sh
-
+    && cd /home/$(whomai)/.dotfiles \
+    && sh install.sh
 
 dialog --infobox "[$(whoami)] Install composer global tools" 10 60
 
