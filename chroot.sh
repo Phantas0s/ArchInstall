0c209ad497a9ea342532b296f5b2deb3942ee70d
Matthieu <Matthieu.cneude@gmail.com>
2018-04-19 08:39:29 +0200

    [master] Add the boot...

--- chroot.sh
+++ chroot.sh
@@ -10,8 +10,7 @@ echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "en_US ISO-8859-1" >> /etc/locale.gen
 locale-gen
 
-# Install netctl (from arch iso base) ?
-# TODO take care of the network management
+pacman --noconfirm --needed -S grub && grub-install /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg
 
 pacman --noconfirm --needed -S networkmanager
 systemctl enable NetworkManager
