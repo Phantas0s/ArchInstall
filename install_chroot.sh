565d3ad050460b159edd20adf26d7f393e334dd1
matthieu <matthieu.cneude@gmail.com>
2018-06-24 07:35:08 +0200

    [master] Delete the attempt to provide an interface when setting root password

--- install_chroot.sh
+++ install_chroot.sh
@@ -1,17 +1,5 @@
 # Set the root password
-pass1=$(dialog --no-cancel --passwordbox "Enter your root password" 10 60 3>&1 1>&2 2>&3 3>&1)
-pass2=$(dialog --no-cancel --passwordbox "Enter your root password again. To be sure..." 10 60 3>&1 1>&2 2>&3 3>&1)
-
-while [ $pass1 != $pass2 ]
-do
-    pass1=$(dialog --no-cancel --passwordbox "Passwords do not match.\n\nEnter root password again." 10 60 3>&1 1>&2 2>&3 3>&1)
-    pass2=$(dialog --no-cancel --passwordbox "Retype root password." 10 60 3>&1 1>&2 2>&3 3>&1)
-    unset pass2
-done
-cat <<EOF | passwd
-$pass1
-$pass1
-EOF
+passwd
 
 # Set the timezone
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
