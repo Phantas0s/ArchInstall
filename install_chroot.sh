69962c7a1f0fdd72955dec5c90dc06aa77781bf5
Matthieu <matthieu.cneude@gmail.com>
2019-12-05 11:49:00 +0100

    Debugging

--- install_chroot.sh
+++ install_chroot.sh
@@ -14,10 +14,13 @@ locale-gen
 
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
+# Install dialog for chroot
+pacman --noconfirm --needed -S dialog
+
 dialog --infobox "Install grub for boot..." 4 40
 
 # Install boot
-pacman --noconfirm --needed -S grub && grub-install /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg
+pacman --noconfirm --needed -S grub && grub-install --target=i386-pc /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg
 
 # For encrypted home
 # sed -i -e 's/GRUB_CMDLINE_LINUX="\(.\+\)"/GRUB_CMDLINE_LINUX="\1 cryptdevice=\/dev\/sda4:crypt"/g' -e 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cryptdevice=\/dev\/sda4:crypt"/g' /etc/default/grub
@@ -26,9 +29,6 @@ pacman --noconfirm --needed -S grub && grub-install /dev/sda && grub-mkconfig -o
 # echo "home /dev/sda4 /etc/luks-keys/home" >> /etc/crypttab
 # echo "/dev/mapper/home      /home               ext4    defaults,errors=remount-ro  0  2" >> /etc/fstab
 
-# Install dialog for chroot
-pacman --noconfirm --needed -S dialog
-
-dialog --title "Continue installation" --yesno "Do you want to install all the softwares and the dotfiles?" 15 60 \
-    && curl -LO https://raw.githubusercontent.com/Phantas0s/ArchInstall/master/install_root.sh \
-    && sh ./install_root.sh
+# dialog --title "Continue installation" --yesno "Do you want to install all the softwares and the dotfiles?" 15 60 \
+    # && curl -LO https://raw.githubusercontent.com/Phantas0s/ArchInstall/master/install_root.sh \
+    # && sh ./install_root.sh
