06f32aa453ed9f2547c20679394b0557cfd64ba9
matthieu <matthieu.cneude@gmail.com>
2020-06-18 17:48:01 +0200

    Improve the install further

--- install_sys.sh
+++ install_sys.sh
@@ -3,27 +3,35 @@
 # inspired by aui
 # to install: wget ow.ly/wnFgh -O aui.zip && mkdir aui && bsdtar -x -f aui.zip -C aui
 
+
 dialog --defaultno --title "Are you sure?" --yesno "This is my personnal arch linux install. \n\n\
-    It will just DESTROY EVERYTHING on your hard disk (/dev/sda). \n\n\
+    It will just DESTROY EVERYTHING on your hard disk ($hd). \n\n\
     Don't say YES if you are not sure about what your are doing! \n\n\
     Are you sure?"  15 60 || exit
 
 dialog --no-cancel --inputbox "Enter a name for your computer." 10 60 2> comp
 
+hd=${hd:-/dev/sda}
+select_device() {
+    devices_list=($(lsblk -d | awk '{print "/dev/" $1 " " $4 " off"}' | grep -E 'sd|hd|vd|nvme|mmcblk'))
+    hd=(dialog --title "Choose your hard drive" \
+        --radiolist "Where do you want to install your new system?\n\nSelect with SPACE.\n\nWARNING: Everything will be DESTROYED on the hard disk!" 15 60 4 ${devices_list[@]} --output-fd 1)
+}
+
 hderaser=$(dialog --no-cancel \
---title "!!! DELETE EVERYTHING !!!" \
---menu "Choose the way to destroy everything on your hard disk (/dev/sda)" 15 60 4 \
-1 "Use dd (wipe all disk)" \
-2 "Use schred (slow & secure)" \
-3 "No need - my hard disk is empty" --output-fd 1)
+    --title "!!! DELETE EVERYTHING !!!" \
+    --menu "Choose the way to destroy everything on your hard disk ($hd)" 15 60 4 \
+    1 "Use dd (wipe all disk)" \
+    2 "Use schred (slow & secure)" \
+    3 "No need - my hard disk is empty" --output-fd 1)
 
 dialog --no-cancel --inputbox "You need four partitions: Boot, Swap, Root and Home. \n\n\
     Boot will be 200M.\n\n\
     Enter partitionsize in gb, separated by space for root & swap.\n\n\
     If you dont enter anything: \n\
-    root -> 40G \n\
-    swap -> 16G \n\n\
-    Home will take the rest of the space available" 20 60 2> psize
+        root -> 40G \n\
+        swap -> 16G \n\n\
+        Home will take the rest of the space available" 20 60 2> psize
 
 IFS=' ' read -ra SIZE <<< $(cat psize)
 
@@ -32,11 +40,9 @@ if ! [ ${#SIZE[@]} -eq 2 ] || ! [[ ${SIZE[0]} =~ $re ]] || ! [[ ${SIZE[1]} =~ $r
     SIZE=(40 16);
 fi
 
-dialog --infobox "Formatting /dev/sda..." 4 40
-
 case $hderaser in
-	1) dd if=/dev/zero of=/dev/sda status=progress;;
-	2) shred -v /dev/sda;;
+    1) dialog  dd if=/dev/zero of=$hd status=progress | dialog --title "Formatting $hd..." --progressbox --stdout 20 60;;
+    2) shred -v $hd | dialog --title "Formatting $hd..." --progressbox --stdout 20 60;;
     3) ;;
 esac
 
@@ -49,7 +55,7 @@ timedatectl set-ntp true
 #p - primary partition
 #e - extended partition
 #w - write the table to disk and exit
-cat <<EOF | fdisk /dev/sda
+cat <<EOF | fdisk $hd
 o
 n
 p
@@ -74,18 +80,18 @@ w
 EOF
 partprobe
 
-mkfs.ext4 /dev/sda3
-mkfs.ext4 /dev/sda1
-mkswap /dev/sda2
-swapon /dev/sda2
-mount /dev/sda3 /mnt
+mkfs.ext4 "${hd}3"
+mkfs.ext4 "${hd}1"
+mkswap "${hd}2"
+swapon "${hd}2"
+mount "${hd}3" /mnt
 mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
+mount "${hd}1" /mnt/boot
 
 # to comment to come back to home partition encrypted
-mkfs.ext4 /dev/sda4
+mkfs.ext4 "${hd}4"
 mkdir /mnt/home
-mount /dev/sda4 /mnt/home
+mount "${hd}4" /mnt/home
 
 # dialog --infobox "Encrypt /home partition..." 4 40
 
@@ -121,12 +127,12 @@ cat comp > /mnt/etc/hostname \
     && rm comp
 
 dialog --title "Reboot time" \
---yesno "Congrats! The install is done! \n\nTo run the new graphical environment, you need to restart your computer, log in and type \"startx\" \n\n You should restart your computer before trying your new shiny system. Do you want to restart now?" 20 60
+    --yesno "Congrats! The install is done! \n\nTo run the new graphical environment, you need to restart your computer. \n\nDo you want to restart now?" 20 60
 
 response=$?
 case $response in
-   0) reboot;;
-   1) clear;;
+    0) reboot;;
+    1) clear;;
 esac
 
 clear
