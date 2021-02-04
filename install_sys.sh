b715ec58fd2de9933df6fe083d87e64da881b763
matthieu <matthieu.cneude@gmail.com>
2021-02-04 17:57:24 +0100

    Add dictd and tldr + some minor fixes

--- install_sys.sh
+++ install_sys.sh
@@ -20,7 +20,7 @@ dialog --defaultno \
     --title "Are you sure?" \
     --yesno "This is my personnal arch linux install. \n\n\
     It will just DESTROY EVERYTHING on the hard disk of your choice. \n\n\
-    Don't say YES if you are not sure about what your are doing! \n\n\
+    Don't say YES if you are not sure about what you're doing! \n\n\
     Are you sure?"  15 60 || exit
 
 dialog --no-cancel --inputbox "Enter a name for your computer." 10 60 2> hn
