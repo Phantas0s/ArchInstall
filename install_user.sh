d83c8558b953105ed80f5c8a06039c8c981c7e0b
matthieu <matthieu.cneude@gmail.com>
2021-06-26 20:23:01 +0200

    Refactor install_user

--- install_user.sh
+++ install_user.sh
@@ -1,24 +1,73 @@
 #!/bin/bash
 
-source /github_defaults
+run() {
+    output=$(cat /var_output)
 
-mkdir -p "/home/$(whoami)/Documents"
-mkdir -p "/home/$(whoami)/Downloads"
+    log INFO "FETCH VARS FROM FILES" "$output"
+    url_installer=$(cat /var_url_installer)
+    dry_run=$(cat /var_dry_run)
 
-mkdir -p "/home/$(whoami)/workspace"
-mkdir -p "/home/$(whoami)/composer"
+    log INFO "CREATE DIRECTORIES" "$output"
+    create-default-directories
+    create-go-directories
+    create-nextcloud-directory
+    log INFO "INSTALL YAY" "$output"
+    install-yay "$output"
+    log INFO "INSTALL AUR APPS" "$output"
+    install-aur-apps "$output"
+    log INFO "INSTALL DOTFILES" "$output"
+    install-dotfiles "$url_installer"
+}
 
-command -v "go" >/dev/null && mkdir -p "/home/$(whoami)/workspace/go/bin"
-command -v "go" >/dev/null && mkdir -p "/home/$(whoami)/workspace/go/pkg"
-command -v "go" >/dev/null &&  mkdir -p "/home/$(whoami)/workspace/go/src"
-command -v "nextcloud" >/dev/null && mkdir -p "/home/$(whoami)/Nextcloud"
+log() {
+    local -r level=${1:?}
+    local -r message=${2:?}
+    local -r output=${3:?}
+    local -r timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
-# Activate netctl
-sudo systemctl enable netctl > /dev/null
-sudo systemctl start netctl > /dev/null
+    echo -e "${timestamp} [${level}] ${message}" >>"$output"
+}
+
+create-default-directories() {
+    mkdir -p "/home/$(whoami)/Documents"
+    mkdir -p "/home/$(whoami)/Downloads"
+
+    mkdir -p "/home/$(whoami)/workspace"
+    mkdir -p "/home/$(whoami)/composer"
+}
+
+create-go-directories() {
+    command -v "go" >/dev/null && mkdir -p "/home/$(whoami)/workspace/go/bin"
+    command -v "go" >/dev/null && mkdir -p "/home/$(whoami)/workspace/go/pkg"
+    command -v "go" >/dev/null &&  mkdir -p "/home/$(whoami)/workspace/go/src"
+}
+
+create-nextcloud-directory() {
+    command -v "nextcloud" >/dev/null && mkdir -p "/home/$(whoami)/Nextcloud"
+}
+
+install-yay() {
+    local -r output=${1:?}
+
+    dialog --infobox "[$(whoami)] Installing \"yay\", an AUR helper..." 10 60
+    aur-check "$prog" "$output"
+}
+
+install-aur-apps() {
+    local -r output=${1:?}
+
+    count=$(wc -l < /tmp/aur_queue)
+    c=0
+    cat /tmp/aur_queue | while read -r prog
+    do
+        c=$(( "$c" + 1 ))
+        dialog --infobox "[$(whoami)] AUR install - Downloading and installing program $c out of $count: $prog..." 10 60
+        aur-check "$prog" "$output"
+    done
+}
 
 #Install an AUR package manually.
-aur_install() {
+aur-install() {
     curl -O "https://aur.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz" \
     && tar -xvf "$1.tar.gz" \
     && cd "$1" \
@@ -27,38 +76,36 @@ aur_install() {
     && rm -rf "$1" "$1.tar.gz" ;
 }
 
-#aur_check runs on each of its arguments, if the argument is not already installed, it either uses yay to install it, or installs it manually.
-aur_check() {
+#a ur_check runs on each of its arguments
+# if the argument is not already installed
+# it either uses yay to install it
+# or installs it manually.
+aur-check() {
+    local -r output=${1:?}
+
     qm=$(pacman -Qm | awk '{print $1}')
     for arg in "$@"
     do
         if [[ "$qm" != *"$arg"* ]]; then
-            yay --noconfirm -S "$arg" &>> /tmp/aur_install_log || aur_install "$arg" &>> /tmp/aur_install_log
+            yay --noconfirm -S "$arg" &>> "$output" || aur-install "$arg" &>> "$output"
         fi
     done
 }
 
-cd /tmp
-dialog --infobox "[$(whoami)] Installing \"yay\", an AUR helper..." 10 60
-aur_check yay
 
-count=$(wc -l < /tmp/aur_queue)
-c=0
-cat /tmp/aur_queue | while read -r prog
-do
-    c=$(( "$c" + 1 ))
-    dialog --infobox "[$(whoami)] AUR install - Downloading and installing program $c out of $count: $prog..." 10 60
-    aur_check "$prog"
-done
+install-dotfiles() {
+    local -r url_installer=${1:?}
 
+    DOTFILES="/home/$(whoami)/.dotfiles"
+    if [ ! -d "$DOTFILES" ];
+        then
+            dialog --infobox "[$(whoami)] Downloading .dotfiles..." 10 60
+            git clone --recurse-submodules "$url_installer/.dotfiles.git" "$DOTFILES" >/dev/null
+    fi
 
-DOTFILES="/home/$(whoami)/.dotfiles"
-if [ ! -d "$DOTFILES" ];
-    then
-        dialog --infobox "[$(whoami)] Downloading .dotfiles..." 10 60
-        git clone --recurse-submodules https://github.com/$GITHUB_DOTFILES_USER/"$GITHUB_DOTFILES_NAME".git "$DOTFILES" >/dev/null
-fi
+    source "/home/$(whoami)/.dotfiles/zsh/zshenv"
+    cd "$DOTFILES"
+    command -v "zsh" >/dev/null && zsh ./install.sh -y
+}
 
-source "/home/$(whoami)/.dotfiles/zsh/zshenv"
-cd "$DOTFILES"
-command -v "zsh" >/dev/null && zsh ./install.sh -y
+run "$@"
